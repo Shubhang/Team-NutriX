7bba78f643b7f3f8c1ef11486bcd1ef888b93aee
josephineprickett <57304743+josephineprickett@users.noreply.github.com>
2020-07-27 22:39:24 -0400

    Create dietres.py

---
+++ dietres.py
@@ -0,0 +1,22 @@
+#lactose intolerance
+lac = [milk, cheese, dairy]
+
+#gluten intolerance (celiac)
+cel = [gluten, wheat]
+
+#diabetes
+db = [sugar]
+
+#high blood pressure
+hbp = [sodium]
+
+#various allergens 
+allergens = [nuts, soy, egg, fish]
+
+#religious/cultural restrictions
+#http://www.halalrc.org/images/Research%20Material/Literature/Guide%20to%20Halal%20Foods.pdf
+halah = [alcohol, pork, bacon, vanilla]
+kosher = [pork, rabbit, catfish, sturgeon, shellfish]
+
+#vegetarian
+veg = [beef, fish]
