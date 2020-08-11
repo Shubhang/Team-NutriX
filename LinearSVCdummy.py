a77d0424312997e9507df949226c441fe6e0c5ed
Proofman <pakphum82542@gmail.com>
2020-08-11 23:00:56 +0700

    Update LinearSVCdummy.py
    Adapted some of Josephine's code
    use with NEW-data_recipe.csv
    LSVC Accurary: 0.9170867119585069

--- LinearSVCdummy.py
+++ LinearSVCdummy.py
@@ -54,8 +54,20 @@ dietres = ['diabetes','highbloodpressure','halah','celiac','allergens','lactosei
 X = dataset.drop(dietres+['recipe_id','recipe_name'],axis=1)
 y = dataset['diabetes']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
-    
+
+cols = X_train.columns
+from sklearn.preprocessing import RobustScaler
+
+scaler = RobustScaler()
+
+X_train = scaler.fit_transform(X_train)
+
+X_test = scaler.transform(X_test)
+X_train = pd.DataFrame(X_train, columns=[cols])
+X_test = pd.DataFrame(X_test, columns=[cols])
+
 LSVC = LinearSVC()
 LSVC.fit(X_train,y_train)
 y2_LSVC_model = LSVC.predict(X_test)
 print("LSVC Accurary:",accuracy_score(y_test,y2_LSVC_model)) 
+print('Training-set accuracy score: {0:0.4f}'. format(accuracy_score(y_train, y2_LSVC_model)))
