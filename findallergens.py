2e963074d7b8f4a07df46583a99a8338df93957e
Proofman <pakphum82542@gmail.com>
2020-10-04 19:30:35 +0700

    Update findallergens.py

--- findallergens.py
+++ findallergens.py
@@ -4,20 +4,33 @@ import csv
 #dietary restrictions
 dietres = {}
 
+
+
 #lactose intolerance
 dietres["lactose intolerance"] = ["milk", "cheese", "dairy"]
-        
+
 #gluten intolerance (celiac)
 dietres["celiac"] = ["gluten", "wheat"]
+
+#soybean allergy
+dietres["soy"] = ["soybean", "soy"]
+
+#fish allergy 
+dietres["seafood and fish"] = ["crab", "shrimp", "lobster", "crawfish", "salmon", "cod", "trout", "tuna","fish" , "tilapia", "anchovie", "barracuda", "basa", "bass", "black cod", "blowfish","brill", "catfish", "eel", "flounder", "haddock", "hake", "halibut", "herring", "mackerel", "monkfish", "mullet", "pollock", "sardine", "sea bass", "shad", "swordfish", "trout", "whitefish", "caviar", "crayfish", "clam", "mussel", "octopus", "oyster", "periwinkle", "scallop", "squid", "conch" ]
 
 #diabetes
 dietres["diabetes"] = ["sugar", "sucrose", "fructose"]
 
 #high blood pressure
-dietres["high blood pressure"] = ["sodium", "salt"]
+dietres["high blood pressure"] = ["sodium", "salt","sugar"]
 
+#treenuts 
+dietres["treenut"] = ["filbert", "hazelnut", "almond", "brazil nut", "chestnut", "hickory", "macadamia", "pecan", "pine" , "pistachio" , "walnut"]
+
+#nuts
+dietres["nut"]= dietres["treenut"] + ["peanut", "yeheb", "beech", "oak", "cashew"]
 #various allergens 
-dietres["allergens"] = ["nuts", "soy", "egg", "fish"]
+dietres["allergens"] = ["nuts",  "egg"]
 
 #religious/cultural restrictions
 #http://www.halalrc.org/images/Research%20Material/Literature/Guide%20to%20Halal%20Foods.pdf
@@ -25,11 +38,10 @@ dietres["halah"] = ["alcohol", "pork", "bacon", "vanilla"]
 dietres["kosher"] = ["pork", "rabbit", "catfish", "sturgeon", "shellfish"]
 
 #vegetarian
-dietres["vegetarian"] = ["beef", "fish", "goat", "lamb", "meatball", "pork", "poultry", "chicken", "sausage", "crab", "lobster"]
+dietres["vegetarian"] = ["beef", "goat", "lamb", "meatball", "pork", "poultry", "chicken", "sausage","meat"]+dietres["seafood and fish"]
 
 #veganism
 dietres["vegan"] = dietres["vegetarian"] + dietres["lactose intolerance"] + ["egg"]
-
 new_member = []
 """
 # This code for NEW-ingredientsv1
@@ -71,23 +83,30 @@ with open('NEW-ingredientsv2.csv','w',encoding="utf8",newline='') as file:
         writer.writerow(i)
 """
 #This code below is for NEW-data_recipe.csv
-with open("NEW-data_recipe.csv",encoding="utf8") as file:
+with open("data/New_3rd_data.csv",encoding="utf8") as file:
     i=0
     reader = csv.reader(file)
     for row in reader:
         if i==0:
             new = row.copy()
-            new.append("Allergens")
+            #new.append("txt")
+            new_member.append(new)
+            i+=1
+            continue
         else:
             txt = row[2].split(",")
             allergens=[]
+            back=[]
             for j in txt:
-                tocheck =j.lower()
+                tocheck = j.lower().split()
                 for k in dietres:
                     for l in dietres[k]:
-                        if(l in tocheck):
-                            allergens.append(k)
-                            break
+                        for p in tocheck:    
+                            if(l == p and k not in allergens):
+                                #back.append(l)
+                                #back.append(tocheck)
+                                allergens.append(k)
+                                break
             txt =""
             length = len(allergens)
             for j in range(length):
@@ -97,11 +116,12 @@ with open("NEW-data_recipe.csv",encoding="utf8") as file:
             new = row.copy()
             if(txt ==""):
                 txt = "NONE"
-            new.append(txt)
+            new[5]=txt
+            #new.append(back)
         i+=1
         new_member.append(new)
 
-with open("NEW-data_recipev2.csv",'w',encoding="utf8",newline="") as file:
+with open("data/New_3rd_datav2.csv",'w',encoding="utf8",newline="") as file:
     writer = csv.writer(file)
     for i in new_member:
         writer.writerow(i)
